8cf376cbdecbfa3d9c61e5ac4312684951e96b37
Margaux REVELLE MONTEIRO <margaux.revelle@esme.fr>
2024-11-08 12:17:42 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/mrevellemonteiro/a2msiproject/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.mrevellemonteiro.a2msiproject
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.mrevellemonteiro.a2msiproject", appContext.packageName)
+    }
+}
