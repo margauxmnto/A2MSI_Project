ea4e300799f57689307d982feb731e327b615c1b
bernoldwong <bernold.wong@esme.fr>
2024-12-02 16:46:23 +0100

    Création du splash screen activity. Maintenant il faut faire en sorte que l'animation occupe tout l'écran et que le passage du splash screen au main activity ne se fasse pas avec un timer maais une détection de click ou pression

--- app/src/main/java/com/mrevellemonteiro/a2msiproject/SplashScreenActivity.kt
+++ app/src/main/java/com/mrevellemonteiro/a2msiproject/SplashScreenActivity.kt
@@ -5,16 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.*
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.mrevellemonteiro.a2msiproject.ui.theme.A2MSIProjectTheme
 
 class SplashScreenActivity : ComponentActivity() {
@@ -36,7 +35,7 @@ class SplashScreenActivity : ComponentActivity() {
 @Composable
 fun SplashScreen(onTimeout: () -> Unit) {
     // Chargez l'animation Lottie à partir du fichier JSON dans les ressources raw
-    val compositionResult = rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.anim_logo))
+    val compositionResult = rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.anim_logov2))
     val progress = animateLottieCompositionAsState(compositionResult.value)
 
     // Interface du Splash Screen
@@ -44,11 +43,6 @@ fun SplashScreen(onTimeout: () -> Unit) {
         modifier = Modifier
             .fillMaxSize()
             .clickable(
-                interactionSource = remember { MutableInteractionSource() }, // Utilisez remember ici
-                indication = androidx.compose.foundation.ripple.rememberRipple(
-                    bounded = false, // Effet ripple non limité au composant
-                    color = MaterialTheme.colorScheme.primary // Utilise la couleur primaire pour le ripple
-                ),
                 onClick = { onTimeout() } // Naviguer vers MainActivity au clic
             ),
         contentAlignment = Alignment.Center
